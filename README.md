7c0cb4281d8d435af6b2bbb57dc371e044efd1b3
Asheesh Laroia <asheesh@asheesh.org>
2014-05-11 15:33:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+World's most trivial program to tell Asheesh if we need to add
+people to the oh-mainline committers group.
+
+License: WTFPL.
---
+++ ok.py
@@ -0,0 +1,38 @@
+import requests
+import collections
+
+
+def get_data():
+    r = requests.get('https://api.github.com/repos/openhatch/oh-mainline/pulls?state=all&per_page=100')
+    return r.json()
+
+def just_merged_pull_requests(response):
+    return [x
+       for x in response
+       if x['merged_at']]
+
+def filter_where_two_frome_same_user(response):
+    users_to_count = collections.defaultdict(int)
+    for x in response:
+        users_to_count[x['user']['login']] += 1
+    return [user for user in users_to_count
+            if users_to_count[user] >= 2]
+
+users_already_added = set(['willingc'])
+
+def main():
+    found_people_to_add = False
+
+    r = get_data()
+    people = filter_where_two_frome_same_user(
+        just_merged_pull_requests(r))
+    for person in people:
+        if person not in users_already_added:
+            found_people_to_add = True
+            print 'Maybe you should add', person
+    return found_people_to_add
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
